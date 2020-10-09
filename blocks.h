d02590e9e04d7736cba39d4c7034755c90f8f9d2
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-10-09 19:20:48 +0000

    rounding off cpu

--- blocks.h
+++ blocks.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"  ", "uname -r",					0,		8},
 	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		8},
-	{" ", "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage \"%\"}'",					5,		14},
+	{" ", "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print int(usage) \"%\"}'",					5,		14},
 
 	{" " , "date '+%b %d (%a) %I:%M%p'",					60,		1},
 	//{"ﲊ ", "free --mebi | sed -n '2{p;q}' | awk '{printf (\"%2.2fGiB/%2.2fGiB\n\", ( $3 / 1024), ($2 / 1024))}'",     5,      14},
